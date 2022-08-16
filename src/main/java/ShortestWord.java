75a699bb68cffc6f2607e49ae5a7dce87b147f03
Andrey Laktyushin <a.laktyushin@aznresearch.com>
2022-08-16 14:55:28 +0300

    https://www.codewars.com/kata/57cebe1dc6fdc20c57000ac9/train/java

--- src/main/java/ShortestWord.java
+++ src/main/java/ShortestWord.java
@@ -1,7 +1,7 @@
 import java.util.Arrays;
 import java.util.Comparator;
 
-public class Kata {
+public class ShortestWord {
     public static int findShort(String s) {
         return Arrays.stream(s.split(" ")).min(Comparator.comparing(String::length)).get().length();
     }
