29c4982ec2a9f0a8bd54623f60aa1a13ba79d0dd
Andrey Laktyushin <a.laktyushin@aznresearch.com>
2022-08-16 17:06:40 +0300

    https://www.codewars.com/kata/523f5d21c841566fde000009/train/java

---
+++ src/main/java/ArrayDiff.java
@@ -0,0 +1,18 @@
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class ArrayDiff {
+
+    public static int[] arrayDiff(int[] a, int[] b) {
+        List<Integer> aaa = Arrays.stream(a).boxed().collect(Collectors.toList());
+        List<Integer> bbb = Arrays.stream(b).boxed().collect(Collectors.toList());
+        aaa.removeAll(bbb);
+        int[] result = new int[aaa.size()];
+        for (int i = 0; i < aaa.size(); i++) {
+            result[i] = Integer.parseInt(String.valueOf(aaa.get(i)));
+        }
+        return result;
+    }
+}
