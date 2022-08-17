8c3b9878576312c7a0bf5995f45821ef4e42053b
Andrey Laktyushin <a.laktyushin@aznresearch.com>
2022-08-17 12:24:58 +0300

    https://www.codewars.com/kata/5266876b8f4bf2da9b000362/train/java

--- src/main/java/Katas/WhoLikesIt.java
+++ src/main/java/Katas/WhoLikesIt.java
@@ -3,7 +3,13 @@ package Katas;
 public class WhoLikesIt {
 
     public static String whoLikesIt(String... names) {
-        //Do your magic here
-        return "";
+        return switch (names.length) {
+            case 0 -> "no one likes this";
+            case 1 -> names[0].concat(" likes this");
+            case 2 -> names[0].concat(" and ").concat(names[1].concat(" like this"));
+            case 3 -> names[0].concat(", ").concat(names[1]).concat(" and ").concat(names[2].concat(" like this"));
+            default ->
+                    names[0].concat(", ").concat(names[1]).concat(" and ").concat(String.valueOf(names.length - 2)).concat(" others like this");
+        };
     }
 }
